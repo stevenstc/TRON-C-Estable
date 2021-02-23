0d9bb9732ce07d48c2daebefe06b2d9348478645
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-02-23 17:01:31 -0500

    Update page web

---
+++ docs/precache-manifest.78995cde4909ee141525be0555dc6fdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f552b5d7c3d1b7b1a6995954420b654",
+    "url": "/TRON-C-Estable/index.html"
+  },
+  {
+    "revision": "e73edb7645fb4441e4ad",
+    "url": "/TRON-C-Estable/static/js/2.1ff741db.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/TRON-C-Estable/static/js/2.1ff741db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d38a8972bc1b861ac3",
+    "url": "/TRON-C-Estable/static/js/main.4f5e7988.chunk.js"
+  },
+  {
+    "revision": "1d23237202d7c6b92805",
+    "url": "/TRON-C-Estable/static/js/runtime-main.513ada86.js"
+  },
+  {
+    "revision": "d3a8f115f144c07658c6ec16d878680a",
+    "url": "/TRON-C-Estable/static/media/TronLinkLogo.d3a8f115.png"
+  }
+]);
